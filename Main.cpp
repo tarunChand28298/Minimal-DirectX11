1948ac1ee51b8a1c365fff938e129043fada5a3d
tarunChand28298 <44527962+tarunChand28298@users.noreply.github.com>
2019-02-25 23:16:48 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,6 @@
-#include <fstream>
-#include <vector>
 #include <Windows.h>
 #include <d3d11.h>
+#include <d3dcompiler.h>
 
 struct VERTEX {
 	float x;
@@ -10,45 +9,47 @@ struct VERTEX {
 };
 
 #pragma region Global Variables:
-	bool isRunning = false;
-	constexpr int windowWidth = 800;
-	constexpr int windowHeight = 600;
-	VERTEX verticies[] = { { 0.0f, 0.5f, 0.0f }, { 0.45f, -0.5f, 0.0f }, { -0.45f, -0.5f, 0.0f }, };
+bool isRunning = false;
+constexpr int windowWidth = 800;
+constexpr int windowHeight = 600;
+VERTEX verticies[] = { { 0.0f, 0.5f, 0.0f }, { 0.45f, -0.5f, 0.0f }, { -0.45f, -0.5f, 0.0f }, };
 #pragma endregion
 
 LRESULT CALLBACK WindowProcedure(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam) {
-	
+
 	switch (message)
 	{
-		case WM_CLOSE: {
-			DestroyWindow(windowHandle);
-			isRunning = false;
-			break;
-		}
-		default:
-			break;
+	case WM_CLOSE: {
+		DestroyWindow(windowHandle);
+		isRunning = false;
+		break;
 	}
-	
+	default:
+		break;
+	}
+
 	return DefWindowProc(windowHandle, message, wParam, lParam);
 }
 
 int WINAPI WinMain(HINSTANCE appInstance, HINSTANCE prevInst, char* cmdArgs, int cmdShow) {
 
-#pragma region Declerations:
+	#pragma region Declerations:
 	HWND						mainWindowHandle;
 	ID3D11Device*					device = nullptr;
 	ID3D11DeviceContext*				deviceContext = nullptr;
 	IDXGISwapChain*					swapChain = nullptr;
 	ID3D11RenderTargetView*				renderTarget = nullptr;
 	ID3D11Buffer*					vertexBuffer = nullptr;
+	ID3D10Blob*					vertexShaderData = nullptr;
+	ID3D10Blob*					pixelShaderData = nullptr;
 	ID3D11VertexShader*				vertexShader = nullptr;
 	ID3D11PixelShader*				pixelShader = nullptr;
 	ID3D11InputLayout*				inputLayout = nullptr;
-#pragma endregion
+	#pragma endregion
 
 //========================================================================================================================
 
-#pragma region Create Window:
+	#pragma region Create Window:
 	{
 		isRunning = true;
 		WNDCLASS wc = { 0 };
@@ -60,9 +61,9 @@ int WINAPI WinMain(HINSTANCE appInstance, HINSTANCE prevInst, char* cmdArgs, int
 		mainWindowHandle = CreateWindow("ClassName", "Output Window", WS_OVERLAPPED | WS_SYSMENU | WS_MINIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, windowWidth, windowHeight, nullptr, nullptr, appInstance, nullptr);
 		ShowWindow(mainWindowHandle, SW_SHOW);
 	}
-#pragma endregion
+	#pragma endregion
 
-#pragma region Create Device and SwapChain:
+	#pragma region Create Device and SwapChain:
 	{
 		DXGI_SWAP_CHAIN_DESC scd = { 0 };
 		scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
@@ -74,9 +75,9 @@ int WINAPI WinMain(HINSTANCE appInstance, HINSTANCE prevInst, char* cmdArgs, int
 
 		D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, nullptr, 0, D3D11_SDK_VERSION, &scd, &swapChain, &device, nullptr, &deviceContext);
 	}
-#pragma endregion
+	#pragma endregion
 
-#pragma region Create Render Target:
+	#pragma region Create Render Target:
 	{
 		ID3D11Texture2D* backBuffer = nullptr;
 		swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&backBuffer);
@@ -85,9 +86,9 @@ int WINAPI WinMain(HINSTANCE appInstance, HINSTANCE prevInst, char* cmdArgs, int
 
 		backBuffer->Release();
 	}
-#pragma endregion
+	#pragma endregion
 
-#pragma region Create Buffer:
+	#pragma region Create Buffer:
 	{
 		D3D11_BUFFER_DESC bd = { 0 };
 		bd.ByteWidth = sizeof(VERTEX) * ARRAYSIZE(verticies);
@@ -97,32 +98,27 @@ int WINAPI WinMain(HINSTANCE appInstance, HINSTANCE prevInst, char* cmdArgs, int
 
 		device->CreateBuffer(&bd, &srd, &vertexBuffer);
 	}
-#pragma endregion
+	#pragma endregion
 
-#pragma region Create Shaders and InputLayout:
+	#pragma region Create Shaders and InputLayout:
 	{
-		using namespace std;
+		D3DReadFileToBlob(L"VertexShader.cso", &vertexShaderData);
+		D3DReadFileToBlob(L"PixelShader.cso", &pixelShaderData);
 
-		ifstream vsf("VertexShader.cso", ios::binary);
-		ifstream psf("PixelShader.cso", ios::binary);
-
-		vector<char> vsd = { std::istreambuf_iterator<char>(vsf), std::istreambuf_iterator<char>() };
-		vector<char> psd = { std::istreambuf_iterator<char>(psf), std::istreambuf_iterator<char>() };
-
-		device->CreateVertexShader(vsd.data(), vsd.size(), nullptr, &vertexShader);
-		device->CreatePixelShader(psd.data(), psd.size(), nullptr, &pixelShader);
+		device->CreateVertexShader(vertexShaderData->GetBufferPointer(), vertexShaderData->GetBufferSize(), nullptr, &vertexShader);
+		device->CreatePixelShader(pixelShaderData->GetBufferPointer(), pixelShaderData->GetBufferSize(), nullptr, &pixelShader);
 
 		deviceContext->VSSetShader(vertexShader, nullptr, 0);
 		deviceContext->PSSetShader(pixelShader, nullptr, 0);
 
 
 		D3D11_INPUT_ELEMENT_DESC ied[] = { {"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0} };
-		device->CreateInputLayout(ied, ARRAYSIZE(ied), vsd.data(), vsd.size(), &inputLayout);
+		device->CreateInputLayout(ied, ARRAYSIZE(ied), vertexShaderData->GetBufferPointer(), vertexShaderData->GetBufferSize(), &inputLayout);
 		deviceContext->IASetInputLayout(inputLayout);
 	}
-#pragma endregion
+	#pragma endregion
 
-#pragma region Setting up Viewport:
+	#pragma region Setting up Viewport:
 	{
 		D3D11_VIEWPORT viewport = { 0 };
 		viewport.TopLeftX = 0;
@@ -132,27 +128,27 @@ int WINAPI WinMain(HINSTANCE appInstance, HINSTANCE prevInst, char* cmdArgs, int
 
 		deviceContext->RSSetViewports(1, &viewport);
 	}
-#pragma endregion
+	#pragma endregion
 
 //========================================================================================================================
 	while (isRunning) {
 
-#pragma region Handle Messages:
+		#pragma region Handle Messages:
 		MSG message = { 0 };
 		if (PeekMessage(&message, mainWindowHandle, 0, 0, PM_REMOVE)) {
 			TranslateMessage(&message);
 			DispatchMessage(&message);
 		}
-#pragma endregion
-		
-#pragma region Clear Viewport:
+		#pragma endregion
+
+		#pragma region Clear Viewport:
 		{
 			float clearColour[4] = { 1.0f, 0.0f, 1.0f, 1.0f };
 			deviceContext->ClearRenderTargetView(renderTarget, clearColour);
 		}
-#pragma endregion
-		
-#pragma region Transfer Data:
+		#pragma endregion
+
+		#pragma region Transfer Data:
 		{
 			UINT stride = sizeof(VERTEX);
 			UINT offset = 0;
@@ -160,28 +156,30 @@ int WINAPI WinMain(HINSTANCE appInstance, HINSTANCE prevInst, char* cmdArgs, int
 			deviceContext->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
 			deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 		}
-#pragma endregion
+		#pragma endregion
 
-#pragma region Render:
+		#pragma region Render:
 		{
 			deviceContext->Draw(3, 0);
 			swapChain->Present(1, 0);
 		}
-#pragma endregion
+		#pragma endregion
 
 	}
-//========================================================================================================================
+	//========================================================================================================================
 
-#pragma region Clean Up:
+	#pragma region Clean Up:
 	device->Release();
 	deviceContext->Release();
 	swapChain->Release();
 	renderTarget->Release();
 	vertexBuffer->Release();
+	vertexShaderData->Release();
+	pixelShaderData->Release();
 	vertexShader->Release();
 	pixelShader->Release();
 	inputLayout->Release();
-#pragma endregion
+	#pragma endregion
 
 	return 0;
 }
